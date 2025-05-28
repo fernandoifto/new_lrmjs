1b085072ed2dd5263d8781a02c72eb3d1b1a9c5b
fernandoifto <fernando.algaworks@gmail.com>
2025-05-28 19:48:43 -0300

    Formulario Agendamento

---
+++ views/src/app/agendar/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../home/components/header';
+import Footer from '../home/components/footer';
+import Form from './components/form';
+
+export default function Agendar() {
+  return (
+    <main>
+      <Header />
+      <Form />
+      <Footer />
+    </main>
+  );
+}
